4ce03ed43b3f89b109dd36fdb8fe98d79874ebaa
hoangkhanh7030 <hoangkhanh7030@gmail.com>
2021-06-29 14:56:36 +0700

    build api get workspace with today

--- src/main/java/com/ces/intern/hr/resourcing/demo/repository/TimeRepository.java
+++ src/main/java/com/ces/intern/hr/resourcing/demo/repository/TimeRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface TimeRepository extends JpaRepository<TimeEntity,Integer> {
 
     @Query(value = "select t from TimeEntity t where t.projectEntity.id=:idProject")
     List<TimeEntity> findAllByIdProject(@Param("idProject") Integer idProject);
+
+    @Query(value = "select * from time where project_id=:projectId and date_part('day',start_time)=:today",nativeQuery = true)
+    List<TimeEntity> findByToday(@Param("today") Integer today, @Param("projectId") Integer projectId);
 
 }
