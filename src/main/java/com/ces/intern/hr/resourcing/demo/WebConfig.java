62dae12dc4b7eb7e086370b3483ec3ce9dd7daee
hoangkhanh7030 <hoangkhanh7030@gmail.com>
2021-06-29 17:07:53 +0700

    fix cors

--- src/main/java/com/ces/intern/hr/resourcing/demo/WebConfig.java
+++ src/main/java/com/ces/intern/hr/resourcing/demo/WebConfig.java
@@ -1,6 +1,13 @@
 package com.ces.intern.hr.resourcing.demo;
 
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,7 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("*");
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*").allowedHeaders("*");
     }
+
+
 }
