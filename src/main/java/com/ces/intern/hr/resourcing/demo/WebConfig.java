ac6475d5f08999162d218c4aaf489082a7155520
hoangkhanh7030 <hoangkhanh7030@gmail.com>
2021-06-29 17:35:10 +0700

    fix cors

--- src/main/java/com/ces/intern/hr/resourcing/demo/WebConfig.java
+++ src/main/java/com/ces/intern/hr/resourcing/demo/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedOrigins("*");
+                .allowCredentials(false).maxAge(3600);
     }
 }
