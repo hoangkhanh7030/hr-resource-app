45f624d70403d01f73c1190b3df01f13c8533652
hoangkhanh7030 <hoangkhanh7030@gmail.com>
2021-07-01 17:43:51 +0700

    fix login

--- src/main/java/com/ces/intern/hr/resourcing/demo/controller/TeamController.java
+++ src/main/java/com/ces/intern/hr/resourcing/demo/controller/TeamController.java
@@ -14,6 +14,7 @@ import com.ces.intern.hr.resourcing.demo.utils.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -45,8 +46,8 @@ public class TeamController {
         if(accountWorkspaceRoleEntity.getCodeRole().equals(Role.EDIT.getCode())) {
             teamService.addResourceToTeam(idTeam,idResource);
             if (resourceRepository.findByIdTeamandIdResource(idTeam,idResource).isPresent()){
-                return new MessageResponse(ResponseMessage.UPDATE_SUCCESS, Status.SUCCESS.getCode());
-            }else return new MessageResponse(ResponseMessage.UPDATE_FAIL,Status.FAIL.getCode());
+                return new MessageResponse(ResponseMessage.ADD_SUCCESS, Status.SUCCESS.getCode());
+            }else return new MessageResponse(ResponseMessage.ADD_FAIL,Status.FAIL.getCode());
         }else return new MessageResponse(ResponseMessage.ROLE,Status.FAIL.getCode());
     }
 }
