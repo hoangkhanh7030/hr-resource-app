3fabd0b4f76754e8715276c7a453ed22acce8b8d
hoangkhanh7030 <hoangkhanh7030@gmail.com>
2021-07-09 09:56:45 +0700

    build api sort

---
+++ src/main/java/com/ces/intern/hr/resourcing/demo/utils/SortPara.java
@@ -0,0 +1,16 @@
+package com.ces.intern.hr.resourcing.demo.utils;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@Getter
+
+@AllArgsConstructor
+public enum SortPara {
+    ASC("ASC"),DESC("DESC");
+    private String name;
+
+
+}
